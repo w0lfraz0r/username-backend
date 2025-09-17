c5a07a2297533b5c30ba7ea4528882741f5b85e9
vaibhav0shrivas <vaibhavshrivasone@gmail.com>
2025-09-17 14:13:48 +0530

    added all origin request

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { pool } from "./db.js";
-import { redis } from "./redis.js";
 import routes from "./routes.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+
+app.use(cors({
+  origin: '*',
+}));
 
 // routes
 app.use("/users", routes);
